047eb8de301e782dc6fd56d946637b4053644f72
Vladislava Babukova <Vlada.409b@gmail.com>
2025-07-20 19:44:17 +0300

    Финальное задание 15 спринта.

--- src/main/java/ru/practicum/shareit/ShareItApp.java
+++ src/main/java/ru/practicum/shareit/ShareItApp.java
@@ -2,7 +2,11 @@ package ru.practicum.shareit;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+@EnableJpaRepositories(basePackages = "ru.practicum.shareit")
+@EntityScan(basePackages = "ru.practicum.shareit")
 @SpringBootApplication
 public class ShareItApp {
 
