8f2507fde535a5acba11ada26281bbd0f6aaac14
Vladislava Babukova <Vlada.409b@gmail.com>
2025-07-20 20:00:48 +0300

    Финальное задание 15 спринта.

--- src/test/java/ru/practicum/shareit/ShareItTests.java
+++ src/test/java/ru/practicum/shareit/ShareItTests.java
@@ -1,22 +1,15 @@
 package ru.practicum.shareit;
 
-import org.apache.catalina.core.ApplicationContext;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
-
-import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
 @ActiveProfiles("test")
 class ShareItTests {
 
-    @Autowired
-    private ApplicationContext context;
-
     @Test
     void contextLoads() {
-        assertNotNull(context);
     }
+
 }
