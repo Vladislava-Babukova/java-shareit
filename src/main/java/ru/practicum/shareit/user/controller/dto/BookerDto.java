18c9c25d99e1ac1222bcb5dc8654e22d3aab9d62
Vladislava Babukova <Vlada.409b@gmail.com>
2025-07-23 16:08:23 +0300

    Фз-15. Исправлены замечания

--- src/main/java/ru/practicum/shareit/user/controller/dto/BookerDto.java
+++ src/main/java/ru/practicum/shareit/user/controller/dto/BookerDto.java
@@ -2,8 +2,6 @@ package ru.practicum.shareit.user.controller.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.Getter;
-import lombok.Setter;
 
 @Data
 @AllArgsConstructor
--- src/main/java/ru/practicum/shareit/user/controller/dto/UserDto.java
+++ src/main/java/ru/practicum/shareit/user/controller/dto/UserDto.java
@@ -3,8 +3,6 @@ package ru.practicum.shareit.user.controller.dto;
 import jakarta.validation.constraints.Email;
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.Getter;
-import lombok.Setter;
 
 @Data
 @AllArgsConstructor
